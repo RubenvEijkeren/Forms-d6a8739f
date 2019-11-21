6600a3481509ad7d0cbc8ad524a8171da343ef83
RubenvEijkeren <54809240+RubenvEijkeren@users.noreply.github.com>
2019-11-21 11:43:59 +0100

    Uitwerking van de exercise

---
+++ form.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>form</title>
+</head>
+<body>
+	<form>
+		<input type="text" name="email" placeholder="Type your email here">
+		<input type="submit" name="submit" value="Submit">
+	</form>
+</body>
+</html>
